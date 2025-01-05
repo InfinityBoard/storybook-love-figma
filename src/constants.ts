a73ffec868cfb7cd10cd93cf5652ed9018ed544c
iamair <iamair@live.com>
2025-01-05 09:37:01 +0500

    feat: fix figma token

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,8 @@ import * as Figma from "figma-js";
 export const ADDON_ID = "storybook-love-figma";
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const KEY = `storybook-love-figma`;
-export const FIGMA_TOKEN = "figd_u1cEwpoxoxXhtK2XIfADsga6eGQdFzorqv9lLfsM";
+export const FIGMA_TOKEN = process.env.STORYBOOK_FIGMA_ACCESS_TOKEN;
 export const FIGMA_CLIENT = Figma.Client({ personalAccessToken: FIGMA_TOKEN });
-// process.env.STORYBOOK_FIGMA_ACCESS_TOKEN;
 
 export const EVENTS = {
   RESULT: `${ADDON_ID}/result`,
