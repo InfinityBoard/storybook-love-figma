636302e7b3223e3001d19afe06daf68493535a43
iamair <iamair@live.com>
2025-01-05 09:40:47 +0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import * as Figma from "figma-js";
 export const ADDON_ID = "storybook-love-figma";
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const KEY = `storybook-love-figma`;
-export const FIGMA_TOKEN = process.env.STORYBOOK_FIGMA_ACCESS_TOKEN;
+export const FIGMA_TOKEN = "figd_GuqoF7IzQVf8Stb0U3Ez2P8hrJYsyr7PmIwqIdLL";
 export const FIGMA_CLIENT = Figma.Client({ personalAccessToken: FIGMA_TOKEN });
 
 export const EVENTS = {
